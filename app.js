c84e6db8273dd2fdf114c54d27efbdb2681e7fdd
TKarasalih <tkarasalih@gmail.com>
2021-07-09 20:48:56 +0100

    Commit with Challenge 1 complete
    Challenge 1 complete and pushed for initial testing

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var fs = require("fs");
+var https = require("https");
+var app = express();
+
+app.use("/challenge-1", express.static("challenge-1"));
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(7000, function () {
+    console.log("App listening on port 7000! Go to https://localhost:7000/");
+  });
---
+++ challenge-1/challenge-1.js
@@ -0,0 +1,65 @@
+/* 
+Create fixed bar div with containers and internal elements
+*/
+// fixed bar
+const fixedBar = document.createElement("div");
+fixedBar.classList.add("fixed-bar__challenge-1");
+// inner container
+const container = document.createElement("div");
+container.classList.add("container__challenge-1");
+// quantity button
+const quant = document.createElement("div");
+quant.classList.add("quant__challenge-1");
+quant.innerHTML = `Qty: <span id="quant-update__challenge-1">1</span> <i class="a-icon a-icon-dropdown icon-pos__challenge-1"></i>`;
+// add to bag (atb)
+const atb = document.createElement("div");
+atb.classList.add("atb__challenge-1");
+atb.innerHTML = `
+<div class="a-button a-button-primary a-button-icon atb-btn__challenge-1">
+  <i class="a-icon a-icon-cart" style="display: block;float:left;"></i>
+  Add to Basket
+</div>
+`;
+
+// append all
+container.appendChild(quant);
+container.appendChild(atb);
+fixedBar.appendChild(container);
+
+document.body.appendChild(fixedBar);
+
+/* 
+Simulate click on buttons
+*/
+// quantity
+quant.onclick = function () {
+  quantSim();
+};
+function quantSim() {
+  document.getElementById("a-autoid-0-announce").click();
+}
+
+// add to bag
+atb.onclick = function () {
+  atbSim();
+};
+function atbSim() {
+  document.getElementById("add-to-cart-button").click();
+}
+
+const targetNode = document.getElementById("mobileQuantitySelection");
+const config = { attributes: true, childList: true, subtree: true };
+
+const callback = function (mutationsList, observer) {
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      // define inner text for quantity numeral
+      var quantNum =
+        document.getElementsByClassName("a-dropdown-prompt")[0].innerText;
+      quant.getElementsByTagName("span")[0].innerText = quantNum;
+    }
+  }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
